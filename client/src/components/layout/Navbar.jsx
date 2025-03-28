865813ea6b3116609c867a3581cecb0cd2869378
ayodeji akinola <ayodejiakinola007@gmail.com>
2025-03-28 22:07:21 +0100

    kk

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -9,7 +9,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Menu,
   X,
